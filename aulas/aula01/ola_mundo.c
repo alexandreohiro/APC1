b8af35328ad9a50739f105cbae03fb8ff4159d2f
Alexandre Vieira <alexandre.v.silva@iesb.edu.br>
2025-02-26 21:41:52 -0300

    codigos da aula01

---
+++ aulas/aula01/ola_mundo.c
@@ -0,0 +1,11 @@
+/*
+Aula-01 Estrutura basica de um programa e C
+*/
+#include <stdio.h> // inclusao de bibliotecas
+
+int main() { // funcao principal que inicia o programa
+    
+    printf("Ola pessoal!\n");
+
+    return 0;
+}
